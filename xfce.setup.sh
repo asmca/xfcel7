2d545da67197ea3a65df0f121ad538e627159c37
Suse Shi <zip@asmca.net>
2015-01-12 19:41:26 +0800

    script now starts

---
+++ xfce.setup.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Why This Script.
+# Just because EPEL 7 repo is not friendly enough for now.
+# Mon Jan 12 19:39:34 CST 2015
+
+
+
+# Please READ this before you begin. 
+# It's not an interactive script. Run it and you will wait for all steps done.
+
+# Please config following ENV. to enable/disable applications which you need or not...
+
+# 
+
+
+
+
+# Whether you modify this option..or not...
+# xfce4 will be installed anyway. Who said it's a script to enable xfce4 on EL7!
+
+XFCE4_ENABLE=1
+
+#
+
+
+
+
+# 1st step. we add a check if EL7 is running. I dont have old EL*/XFCE4 for desktop usages for now. so scripts/rpms may not be ready yet.
+
+
